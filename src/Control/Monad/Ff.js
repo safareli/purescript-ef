d0d3a5f92d30bbd419a61bf5608072013e023042
Irakli Safareli <i.safareli@gmail.com>
2017-12-14 00:56:43 +0100

    fix build

--- src/Control/Monad/Ff.js
+++ src/Control/Monad/Ff.js
@@ -21,36 +21,6 @@ var APPLY = "APPLY";
 var BIND = "BIND";
 var APPLY_FUNC = "APPLY_FUNC";
 
-exports.pureE = function (x) {
-  return mkEf(PURE, x);
-};
-
-exports.mapE = function (f) {
-  return function (eff) {
-    return mkEf(MAP, f, eff);
-  };
-};
-
-exports.applyE = function (effF) {
-  return function (eff) {
-    return mkEf(APPLY, eff, effF);
-  };
-};
-
-exports.bindE = function (eff) {
-  return function (f) {
-    return mkEf(BIND, f, eff);
-  };
-};
-
-
-var mkEf = function (tag, _0, _1) {
-  var eff = function eff_() { return toEff(eff_) }
-  eff.tag = tag
-  eff._0 = _0
-  eff._1 = _1
-  return eff
-}
 
 var toEff = function (inputEff) {
   var operations = [];
@@ -80,7 +50,7 @@ var toEff = function (inputEff) {
         res = op._0(res);
       } else if (op.tag === APPLY) {
         eff = op._0;
-        operations.push(new Ef(APPLY_FUNC, res));
+        operations.push({ tag: APPLY_FUNC, _0: res });
         continue effLoop;
       } else { // op.tag === BIND
         eff = op._0(res);
@@ -89,4 +59,34 @@ var toEff = function (inputEff) {
     }
     return res;
   }
+};
+
+var mkEf = function (tag, _0, _1) {
+  var eff = function eff_() { return toEff(eff_); };
+  eff.tag = tag;
+  eff._0 = _0;
+  eff._1 = _1;
+  return eff;
+};
+
+exports.pureE = function (x) {
+  return mkEf(PURE, x);
+};
+
+exports.mapE = function (f) {
+  return function (eff) {
+    return mkEf(MAP, f, eff);
+  };
+};
+
+exports.applyE = function (effF) {
+  return function (eff) {
+    return mkEf(APPLY, eff, effF);
+  };
+};
+
+exports.bindE = function (eff) {
+  return function (f) {
+    return mkEf(BIND, f, eff);
+  };
 };
